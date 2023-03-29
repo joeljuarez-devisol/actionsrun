30227f917bbf92e4beaaefd8f8e0128a3be8c3db
Joel Juarez <40305419+JoelJuarez@users.noreply.github.com>
2023-03-28 22:16:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ function validateQA (commitValue) {
             finalPath += ".apk"
             
             console.log(`finalPath ---> ${finalPath} <---`); 
+            core.setOutput( "finalPath_apk",`v${finalPath}`);
             
         }
     
